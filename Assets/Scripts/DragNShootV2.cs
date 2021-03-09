cb8e77eef2cdb3e36133efd7910512a6a56effb1
Neet Talapaneni <talapanenipuneet@gmail.com>
2021-03-09 15:26:02 +1100

    Further optimisations to Drag N Shoot

--- Assets/Scripts/DragNShootV2.cs
+++ Assets/Scripts/DragNShootV2.cs
@@ -4,45 +4,47 @@ using UnityEngine;
 
 public class DragNShootV2 : MonoBehaviour
 {
-    [SerializeField] private float power = 10f;
+    [SerializeField] private float power = 5f;
 
     [SerializeField] private Vector2 minPower;
     [SerializeField] private Vector2 maxPower;
     Camera cam;
-
     TrajectoryLineV2 trajectoryLine;
     Vector2 force;
-    Vector3 startPoint;
-    Vector3 endPoint;
+    Vector3 lineStart, lineEnd, dir;
+    float dist;
+    Vector3 camOffset = new Vector3(0, 0, 10);
+    Vector3 charOffset = new Vector3(0, -.2f, 0);
+
+
 
     private void Start()
     {
         trajectoryLine = GetComponent<TrajectoryLineV2>();
         cam = Camera.main;
+
     }
-    private void OnMouseDown()
-    {
-        startPoint = cam.ScreenToWorldPoint(Input.mousePosition);
-        startPoint.z = 15;
-    }
+
+    // private void OnMouseDown()
+    // {
+    //     startPoint = cam.ScreenToWorldPoint(Input.mousePosition);
+    // }
 
     private void OnMouseDrag()
     {
-        Vector3 currentPoint = cam.ScreenToWorldPoint(Input.mousePosition);
-        float currDist = Vector3.Distance(currentPoint, transform.position);
-        currentPoint.z = 15;
-        Vector3 dimXY = currentPoint - transform.position;
-        float diff = dimXY.magnitude;
-        Vector3 lineStart = transform.position + ((dimXY / diff) * currDist * -1);
+        lineStart = this.transform.position + charOffset;
+        Vector3 currentPoint = cam.ScreenToWorldPoint(Input.mousePosition) + camOffset;
+        dir = currentPoint - lineStart;
+        dist = Mathf.Clamp(Vector3.Distance(currentPoint, lineStart), 0, 3);
+        lineEnd = lineStart + dir.normalized * dist * -1;
 
-        trajectoryLine.RenderLine(lineStart, transform.position);
+        trajectoryLine.RenderLine(lineStart, lineEnd);
     }
 
     private void OnMouseUp()
     {
-        endPoint = cam.ScreenToWorldPoint(Input.mousePosition);
-        endPoint.z = 15;
-        force = new Vector2(Mathf.Clamp(startPoint.x - endPoint.x, minPower.x, maxPower.x), Mathf.Clamp(startPoint.y - endPoint.y, minPower.y, maxPower.y));
+        lineEnd = transform.position + dir.normalized * dist;
+        force = new Vector2(Mathf.Clamp(transform.position.x - lineEnd.x, minPower.x, maxPower.x), Mathf.Clamp(transform.position.y - lineEnd.y, minPower.y, maxPower.y));
         GetComponent<Rigidbody2D>().AddForce(force * power, ForceMode2D.Impulse);
         trajectoryLine.EndLine();
     }
